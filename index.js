28dfb02e4343be5466db6e4a674bcf6a917285b9
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-09-16 20:59:41 -0700

    commit tree 9d2418ff89af7d6ff91132c09003e4fc74f7831d

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your Circle class here
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+  // Diameter
+  get diameter() {
+    return this.radius * 2;
+  }
+
+  set diameter(newDiameter) {
+    this.radius = newDiameter / 2;
+  }
+
+  // Circumference
+  get circumference() {
+    return this.diameter * Math.PI;
+  }
+
+  set circumference(newCircumference) {
+    this.radius = newCircumference / Math.PI / 2;
+  }
+
+  // Area
+  get area() {
+    return this.radius ** 2 * Math.PI;
+  }
+
+  set area(newArea) {
+    this.radius = Math.sqrt(newArea / Math.PI);
+  }
+}
